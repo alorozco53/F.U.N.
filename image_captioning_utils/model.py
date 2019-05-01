a66e7d384f1d58574e88166a6a72b3feb45f5535
AlOrozco53 <alorozco.patriot53@gmail.com>
2019-05-01 00:51:47 -0500

    [FIXED] a bug

--- image_captioning_utils/model.py
+++ image_captioning_utils/model.py
@@ -8,8 +8,8 @@ class EncoderCNN(nn.Module):
     def __init__(self, embed_size, cnn_model=models.resnet152):
         """Load the pretrained ResNet-152 and replace top fc layer."""
         super(EncoderCNN, self).__init__()
-        resnet = cnn_model(pretrained=True)
-        modules = list(resnet.children())[:-1]      # delete the last fc layer.
+        self.cnn = cnn_model(pretrained=True)
+        modules = list(self.cnn.children())[:-1]      # delete the last fc layer.
         self.resnet = nn.Sequential(*modules)
         self.linear = nn.Linear(resnet.fc.in_features, embed_size)
         self.bn = nn.BatchNorm1d(embed_size, momentum=0.01)
